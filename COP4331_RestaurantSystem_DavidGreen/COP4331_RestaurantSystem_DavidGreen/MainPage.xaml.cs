3e154867ee305b0887d51ec892a4e36b054a6044
David Green <david.green@Knights.ucf.edu>
2020-09-23 13:04:14 -0400

    Initial Project Setup

---
+++ COP4331_RestaurantSystem_DavidGreen/COP4331_RestaurantSystem_DavidGreen/MainPage.xaml.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace COP4331_RestaurantSystem_DavidGreen
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+    }
+}
